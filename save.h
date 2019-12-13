5b118560a9e17a43579d223836c2cc0d0681db0d
Ahmad Waleed <a.waleedothman@gmail.com>
2019-12-13 07:49:38 +0200

    Merge pull request #1 from AWaleedOthman/abnabdelmonem-patch-1
    Add files via upload

---
+++ save.h
@@ -0,0 +1,7 @@
+void saveGrid(char* ,int ,int,int,int );
+void saveBoxes(int* , int ,int );
+void saveData(int ,int ,int ,int ,int , int );
+void loadGrid(char* , int ,int);
+void loadBoxes(int* , int, int);
+void loadData(int* , int* , int* , int* , int* ,int );
+void fileName(int , char []);
