ce8be45045a427c6e44aa9eeb6a003992b49f67d
abnabdelmonem <54194868+abnabdelmonem@users.noreply.github.com>
2019-12-20 03:25:26 +0200

    DON'T forget semi colons!

--- Dots&Boxes/computer.h
+++ Dots&Boxes/computer.h
@@ -2,5 +2,5 @@ int compSearchNum(char*, int, int*, int *, int);
 int compSearchNumX(int*,int,int,int,int);
 int compSearchTF(int*,int,int*,int*,int);
 void compSearchRc(char*, int, int*, int*);
-void compSearchRcX(char*,int,char*,int,int*,int*)
+void compSearchRcX(char*,int,int*,int,int*,int*);
 void compChoose(int*, int, char*, int, int*, int*);
