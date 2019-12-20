c2323550e31753ab1732ee95b36e6ab89fc56e01
abdelrahmankhaledg <58565775+abdelrahmankhaledg@users.noreply.github.com>
2019-12-20 19:34:20 +0200

    Add files via upload

--- Dots&Boxes/computer.c
+++ Dots&Boxes/computer.c
@@ -5,6 +5,133 @@ if(*(boxes+searchRow*n+searchCol)==2){
 }
 return z;
 }
+int searchForTwoX(int *boxes,int n,int row,int column){//counts the number of twos around giving indices
+int i=row;
+int j=column;
+int z=0;
+            if(i==0){
+                if(j==0){
+                    if((searchForTwo(boxes,n,0,1))==1){
+                       z++;
+                    if((searchForTwo(boxes,n,1,0))==1){
+                        z++;
+                    }
+                    }
+                }
+                else if(j==n-1){
+                    if(searchForTwo(boxes,n,1,n-1)==1){
+                        z++;
+                    }
+                    if(searchForTwo(boxes,n,0,n-2)==1){
+                        z++;
+                    }
+                }
+                else{
+                    if(searchForTwo(boxes,n,0,j+1)==1){
+                    z++;
+                    }
+                    if(searchForTwo(boxes,n,0,j-1)==1){
+                        z++;
+                    }
+                    if(searchForTwo(boxes,n,1,j)==1){
+                        z++;
+                    }
+                }
+            }
+            else if(i==n-1){
+                if(j==0){
+                    if(searchForTwo(boxes,n,n-2,0)==1){
+                            z++;
+                    }
+                    if(searchForTwo(boxes,n,n-1,1)==1){
+                        z++;
+                    }
+                }
+                else if(j==n-1){
+                    if(searchForTwo(boxes,n,n-2,n-1)==1){
+                            z++;
+                    }
+                    if(searchForTwo(boxes,n,n-1,n-2)==1){
+                        z++;
+                    }
+                }
+                else{
+                    if(searchForTwo(boxes,n,n-1,j+1)==1){
+                       z++;
+                    }
+                    if(searchForTwo(boxes,n,n-1,j-1)==1){
+                        z++;
+                    }
+                    if(searchForTwo(boxes,n,n-2,j)==1){
+                        z++;
+                    }
+                }
+            }
+            else if (j==0){
+                if(searchForTwo(boxes,n,i-1,j)==1){
+                    z++;
+                }
+                if(searchForTwo(boxes,n,i+1,j)==1){
+                    z++;
+                }
+                if(searchForTwo(boxes,n,i,j+1)==1){
+                    z++;
+                }
+            }
+            else if (j==n-1){
+                if(searchForTwo(boxes,n,i-1,j)==1){
+                 z++;
+                }
+                if(searchForTwo(boxes,n,i+1,j)==1){
+                    z++;
+                }
+                if(searchForTwo(boxes,n,i,j-1)==1){
+                    z++;
+                }
+            }
+            else{
+                if(searchForTwo(boxes,n,i-1,j)==1){
+                    z++;
+                }
+                if(searchForTwo(boxes,n,i+1,j)==1){
+                    z++;
+                }
+                if(searchForTwo(boxes,n,i,j-1)==1){
+                    z++;
+                }
+                if(searchForTwo(boxes,n,i,j+1)==1){
+                    z++;
+                }
+
+            }
+            return z;
+        }
+int searchForTF(int *boxes,int n,int*compcol,int *comprow,int numSearch){//searches for 3 or 4 with least number of 2's around
+int i,j,z=0,k=0,g=0;
+while(n!=-1){
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(*((boxes+i*n)+j)==numSearch){
+               if(searchForTwoX(boxes,n,i,j)==k){
+                *compcol=2*j+1;
+                *comprow=2*i+1;
+                z=1;
+                break;
+               }
+            }
+            }
+            if(z==1){
+                n=-1;
+                break;
+            }
+        }
+        k++;
+        if(k==5){
+            break;
+        }
+        }
+        return z;
+}
 int compSearchNum(int* boxes,int n ,int* compcol,int * comprow,int numSearch){// numsearch is the number to search for in the small grid
     int i,j,z=0;
     for(i=0;i<n;i++){
@@ -31,95 +158,6 @@ if (((*((boxes+y*n)+x)))==numSearch){
     z=1;
 }
 return z;
-}
-int compSearchTF(int *boxes,int n,int* compcol,int* comprow,int numSearch){//searches for any 3 or 4 without 2 near it in the boxes grid and returns 1 if found
-int i,j,z=0;
-for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-        if((*(boxes+i*n+j))==numSearch){
-            if(i==0){
-                if(j==0){
-                    if((searchForTwo(boxes,n,0,1))!=1&&(searchForTwo(boxes,n,1,0))!=1){
-                        *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                    }
-                }
-                else if(j==n-1){
-                    if(searchForTwo(boxes,n,1,n-1)!=1&&searchForTwo(boxes,n,0,n-2)!=1){
-                        *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                    }
-                }
-                else{
-                    if(searchForTwo(boxes,n,0,j+1)!=1&&searchForTwo(boxes,n,0,j-1)!=1&&searchForTwo(boxes,n,1,j)!=1){
-                        *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                    }
-                }
-            }
-            else if(i==n-1){
-                if(j==0){
-                    if(searchForTwo(boxes,n,n-2,0)!=1&&searchForTwo(boxes,n,n-1,1)!=1){
-                        *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                    }
-                }
-                else if(j==n-1){
-                    if(searchForTwo(boxes,n,n-2,n-1)!=1&&searchForTwo(boxes,n,n-1,n-2)!=1){
-                        *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                    }
-                }
-                else{
-                    if(searchForTwo(boxes,n,n-1,j+1)!=1&&searchForTwo(boxes,n,n-1,j-1)!=1&&searchForTwo(boxes,n,n-2,j)!=1){
-                        *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                    }
-                }
-            }
-            else if (j==0){
-                if(searchForTwo(boxes,n,i-1,j)!=1&&searchForTwo(boxes,n,i+1,j)!=1&&searchForTwo(boxes,n,i,j+1)!=1){
-                    *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                }
-            }
-            else if (j==n-1){
-                if(searchForTwo(boxes,n,i-1,j)!=1&&searchForTwo(boxes,n,i+1,j)!=1&&searchForTwo(boxes,n,i,j-1)!=1){
-                    *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                }
-            }
-            else{
-                if(searchForTwo(boxes,n,i-1,j)!=1&&searchForTwo(boxes,n,i+1,j)!=1&&searchForTwo(boxes,n,i,j-1)!=1&&searchForTwo(boxes,n,i,j+1)!=1){
-                    *compcol=j*2+1;
-                        *comprow=i*2+1;
-                        z=1;
-                        break;
-                }
-            }
-        }
-    }
-    if (z==1){
-        break;
-    }
-}
-    return z;
 }
 void compSearchRc(char* grid,int size,int* compcol,int* comprow){// this function searches for the row and col where the computer will play
     int row= *comprow;
@@ -388,16 +426,10 @@ void compChoose(int *boxes, int n, char *grid, int size , int *comprow, int *com
     if (compSearchNum(boxes,n,compcol,comprow,1)){
         compSearchRc(grid,size,compcol,comprow);
     }
-    else if(compSearchTF(boxes,n,compcol,comprow,3)){
+    else if(searchForTF(boxes,n,compcol,comprow,3)){
         compSearchRcX(grid,size,boxes,n,compcol,comprow);
     }
-    else if (compSearchTF(boxes,n,compcol,comprow,4)){
-        compSearchRcX(grid,size,boxes,n,compcol,comprow);
-    }
-    else if(compSearchNum(boxes,n,compcol,comprow,3)){
-        compSearchRcX(grid,size,boxes,n,compcol,comprow);
-    }
-    else if(compSearchNum(boxes,n,compcol,comprow,4)){
+    else if (searchForTF(boxes,n,compcol,comprow,4)){
         compSearchRcX(grid,size,boxes,n,compcol,comprow);
     }
     else if(compSearchNum(boxes,n,compcol,comprow,2)){
